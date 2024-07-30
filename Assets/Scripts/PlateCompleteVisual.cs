b41066ac4459f817ad4cac5cbd1d45e83d29e1f9
rahul <77121568+RahulTambatUbz@users.noreply.github.com>
2024-07-30 12:59:12 +0530

    Added Plate Recipe Visuals
    Added Plate Visuals of Kitchen Object on Plate make them visible

--- Assets/Scripts/PlateCompleteVisual.cs
+++ Assets/Scripts/PlateCompleteVisual.cs
@@ -1,24 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlateCompleteVisual : MonoBehaviour
 {
-    [SerializeField] PlateKitchenObject plateKitchenObject;
+
     // Start is called before the first frame update
+    [Serializable]
     public struct kitchenObjectSO_Gameobject
     {
         public KitchenObjectSO KitchenObjectSO;
-        public GameObject GameObject;
+        public GameObject gameObject;
 
     }
+
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private List<kitchenObjectSO_Gameobject> kitchenObjectSOGameobjectsList;
     private void Start()
     {
-        //plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+        foreach (kitchenObjectSO_Gameobject kitchenObjectSOGameobject in kitchenObjectSOGameobjectsList)
+        {
+
+
+
+            kitchenObjectSOGameobject.gameObject.SetActive(false);
+
+
+
+        }
     }
 
     private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
     {
-       // e.kitchenObjectSO
+        foreach (kitchenObjectSO_Gameobject kitchenObjectSOGameobject in kitchenObjectSOGameobjectsList)
+        {
+            if (kitchenObjectSOGameobject.KitchenObjectSO == e.kitchenObjectSO)
+            {
+
+                kitchenObjectSOGameobject.gameObject.SetActive(true);
+
+            }
+
+        }
+        // / e.kitchenObjectSO
     }
 }
