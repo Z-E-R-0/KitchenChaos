e88f23d4d9cf2fa292939e1f9de15ca0b5ff71f9
rahul <77121568+RahulTambatUbz@users.noreply.github.com>
2024-07-31 18:48:17 +0530

    Added Delivery Counter
    Added Prefab Mesh
    added Delivery Counter that only accepts Plates

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    // Start is called before the first frame update
+    public override void Interact(Player player)
+    {
+        if(player.HasKitchenObjects())
+        {
+            if(player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                //Only accepts Plates
+                player.GetKitchenObject().DestroySelf();
+
+            }
+           
+
+
+        }
+    }
+}
