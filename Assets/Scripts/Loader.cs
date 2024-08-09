c6c92e462ade5c7474645367dba90c2ef4f9b97c
rahul <77121568+RahulTambatUbz@users.noreply.github.com>
2024-08-09 11:54:11 +0530

    Added Loader Scene and MainMenu Scene

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader 
+{
+    
+    private static Scene targetScene;
+    public enum Scene
+    {
+     MainMenuScene,
+     GameScene,
+     LoadingScene
+
+    }
+
+    public static void Load(Scene targetScene)
+    {
+        Loader.targetScene = targetScene;
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+      
+
+    }
+
+    public static void LoaderCallback()
+    {
+        SceneManager.LoadScene(targetScene.ToString());
+    }
+}
